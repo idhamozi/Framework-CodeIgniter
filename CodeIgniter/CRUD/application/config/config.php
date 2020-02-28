f199808f892ae7ddfb160e7b6e4c53815f546057
idham <king.idham@gmail.com>
2020-02-29 02:06:25 +0700

    Rename File into CRUD

--- CodeIgniter/CRUD/application/config/config.php
+++ CodeIgniter/CRUD/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CodeIgniter/latihan_crud_ci/';
+$config['base_url'] = 'http://localhost/Framework-CodeIgniter/CodeIgniter/CRUD/';
 
 /*
 |--------------------------------------------------------------------------
